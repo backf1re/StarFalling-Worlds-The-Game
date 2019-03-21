039becb87c36bc42b48ed82b61b6ccba70e50a99
backf1re <33880547+backf1re@users.noreply.github.com>
2019-03-21 20:06:22 +0300

    added unity project

---
+++ Assets/Scripts/Keycoltroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keycoltroller : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float transV = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        float transH = Input.GetAxis("Horizontal") * playerSpeed * Time.fixedDeltaTime;
+
+        transform.Translate(new Vector3(transH, transV, 0));
+    }
+}
