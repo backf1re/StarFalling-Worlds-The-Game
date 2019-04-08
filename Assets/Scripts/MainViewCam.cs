870d77e8cba98cbf804d2236eb99489cde62270d
backf1re <backf1re@users.noreply.github.com>
2019-04-08 03:23:47 +0300

    added sample map with one type obstacle

---
+++ Assets/Scripts/MainViewCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainViewCam : MonoBehaviour
+{
+    public Transform mwcTarget;
+    int distance = -10;
+    float lift = 1.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, lift, distance) + mwcTarget.position;
+        transform.LookAt(mwcTarget);
+        
+
+    }
+
+}
---
+++ Assets/Scripts/World_0_map.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World_0_map : MonoBehaviour
+{
+    //Obstacle on map World_0_Map item 000
+    public Transform obstacle01;
+    public int obstacle01Count;
+   
+    private Vector2 mapSize = new Vector2 (50, 50);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateMap();
+    }
+    void generateMap()
+    {
+        for (int i = 0; i < obstacle01Count; i++)
+        {
+            Instantiate(obstacle01, new Vector2(Random.Range(0, mapSize.x),
+                                                  Random.Range(0, mapSize.y)), obstacle01.rotation);
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
