6f7e00ba2bcd77c6e8c27950f6667cdb91adbe9c
backf1re <sqbackfire@gmail.com>
2019-07-14 13:21:18 +0300

    Stats recovery
    Created algo for recovery of HP and AP and added AP usage for movement in StepController script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,18 +15,63 @@ public class Player : MonoBehaviour
     // public int Gold { get; set; } // ingame money will be used from blockchain
     public int Level { get; set; } // Current Level
     public int Rebirth { get; set; } // Current Rebirth
+    public int baseHPrecoverSpeed { get; set; } // Base Hit Points recovery speed
+    public int restHPrecoverSpeed { get; set; } // Hit Points recovery speed during rest
+    public int sleepHPrecoverSpeed { get; set; } // Hit Points recovery speed during sleep
+    public int baseAPrecoverSpeed { get; set; } // Base Action Points recovery speed
+    public int restAPrecoverSpeed { get; set; } // Action Points recovery speed during rest
+    public int sleepAPrecoverSpeed { get; set; } // Action Points recovery speed during sleep
+    public float updateDelta;
 
 
-
-    // Start is called before the first frame update
-    void Start()
+    // Recovery by time
+    void Recover()
     {
-        
+        if (currentHP < maximumHP)
+        {
+            updateDelta += Time.deltaTime;
+            if (updateDelta >= 1f)
+            {
+                updateDelta = 0;
+                currentHP = currentHP + baseHPrecoverSpeed + restHPrecoverSpeed + sleepHPrecoverSpeed;
+            }
+            //Debug.Log(currentHP.ToString() + " HP");
+        }
+        if (currentAP < maximumAP)
+        {
+            updateDelta += Time.deltaTime;
+            if (updateDelta >= 1f)
+            {
+                updateDelta = 0;
+                currentAP = currentAP + baseAPrecoverSpeed + restHPrecoverSpeed + sleepHPrecoverSpeed;
+            }
+            Debug.Log(currentHP.ToString() + " HP" + currentAP.ToString() + " AP");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            updateDelta = 0;
+            currentHP = 1;
+            maximumHP = 10;
+            currentAP = 1;
+            maximumAP = 10;
+            currentQP = 0;
+            currentEP = 0;
+            Level = 1;
+            Rebirth = 0;
+            baseHPrecoverSpeed = 1;
+            restHPrecoverSpeed = 0;
+            sleepHPrecoverSpeed = 0;
+            baseAPrecoverSpeed = 1;
+            restAPrecoverSpeed = 0;
+            sleepAPrecoverSpeed = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Recover();
+        }
 }
--- Assets/Scripts/StepController.cs
+++ Assets/Scripts/StepController.cs
@@ -1,45 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class StepController : MonoBehaviour
 {
     public Transform character01_0;
+    public int currentAP;
+    private Player player;
     public float xStep = 0.5f;
     public float yStep = 0.3f;
     public float transH = 0f;
     public float transV = 0f;
 
-    //Making one move after one key pressed
-    void Update()
+    void Start()
     {
-        //Making one move after one key pressed
-        
-        if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0 )
-        {
-          float transV = 0;
-          float transH = xStep;
-          transform.Translate(new Vector3(transH, transV, 0));
-        }
-        if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
-        {
-            float transV = 0;
-            float transH = -xStep;
-            transform.Translate(new Vector3(transH, transV, 0));
-        }
-        if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0)
-        {
-            float transV = -yStep;
-            float transH = 0;
-            transform.Translate(new Vector3(transH, transV, 0));
-        }
-        if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0)
-        {
-            float transV = yStep;
-            float transH = 0;
-            transform.Translate(new Vector3(transH, transV, 0));
-        }
-
-        
+        player = gameObject.GetComponent<Player>();
+        currentAP = player.currentAP;
     }
+
+        //Making one move after one key pressed
+        void Update()
+    {
+        //Making one move after one key pressed if AP > 1
+        if (player.currentAP > 1)
+        {
+
+            if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+            {
+                float transV = 0;
+                float transH = xStep;
+                player.currentAP --;
+                transform.Translate(new Vector3(transH, transV, 0));
+            }
+            if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
+            {
+                float transV = 0;
+                float transH = -xStep;
+                player.currentAP --;
+                transform.Translate(new Vector3(transH, transV, 0));
+            }
+            if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0)
+            {
+                float transV = -yStep;
+                float transH = 0;
+                player.currentAP --;
+                transform.Translate(new Vector3(transH, transV, 0));
+            }
+            if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0)
+            {
+                float transV = yStep;
+                float transH = 0;
+                player.currentAP --;
+                transform.Translate(new Vector3(transH, transV, 0));
+            }
+        }
+     }
 }
